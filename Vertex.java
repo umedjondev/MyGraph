4a7339a31f794ebf1d214ad1295a0125c40ee6c7
Khayriev Umedjon <127809635+umedjondev@users.noreply.github.com>
2025-11-15 00:22:17 +0500

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,31 +1,107 @@
+package Graphs;
+
+
 import java.util.ArrayList;
-import java.util.List;
 
-public class Vertex {
-    private String name;
-    private List<Edge> outEdge,inEdge;
-    public Vertex(String name,boolean isDirected){
+public class Graph {
+    private boolean isDirected=false;
+    private ArrayList<Vertex> vertices;
+    public Graph(){
+        vertices=new ArrayList<Vertex>();
+    }
+    public Graph(boolean isDirected){
+        this.isDirected=isDirected;
+        vertices=new ArrayList<Vertex>();
+    }
+    public Vertex addVertex(String verName){
+        Vertex newVertex=new Vertex(verName,isDirected);
+        vertices.add(newVertex);
+        return newVertex;
+    }
+    public Edge addEdge(Vertex startVer, Vertex endVer){
+        Edge e=startVer.addOutEdge(endVer);
+        endVer.addInEdge(startVer);
+        return e;
+    }
+    public Edge addEdge(Vertex startVer, Vertex endVer,double w){
+        Edge e=startVer.addOutEdge(endVer,w);
+        endVer.addInEdge(startVer,w);
+        return e;
+    }
+    public void removeEdge(Vertex v1,Vertex v2){
+        v1.removeEdge(v2);
+        v2.removeEdge(v1);
+    }
 
-        this.name=name;
-        outEdge=new ArrayList<>();
-        if(!isDirected)inEdge=outEdge;
-        else inEdge=new ArrayList<>();
+    public void removeVertex(Vertex delVer){
+        if(vertices.indexOf(delVer)!=-1){
+            vertices.remove(delVer);
+            for (Edge e:delVer.getOutEdges()){
+                e.getEnd().removeEdge(delVer);
+
+            }
+        }
+        else System.out.println("Grafda "+delVer.getData()+" verteks mavjud emas");
     }
-    public void addEdgeOut(Vertex v2){
-        Edge e=new Edge(this,v2);
-        outEdge.add(e);
+
+public int getCountEdges(){
+        int count=0;
+        for (Vertex v:vertices){
+            count+=v.getOutEdges().size();
+        }
+        if(isDirected)return count;
+        return count/2;
+}
+
+    public void print(){
+        System.out.println("OutBox ##########################");
+        for (Vertex v:vertices){
+            v.print();
+
+        }
+        System.out.println("_________________________________");
     }
-    public void addEdgeOut(Vertex v2, double w){
+    public void printIn(){
+        System.out.println("InBox  ##########################");
+        for (Vertex v:vertices){
+            v.printIn();
+        }
+        System.out.println("_________________________________");
+    }
+public ArrayList<Vertex> getVertices(){
+        return this.vertices;
+}
+
+public void updateWeight(Vertex v1, Vertex v2, int w){
+        v1.updateWeight(v2,w);
+        v2.updateWeight(v1,w);
+
+}
+public Vertex opposite(Vertex v,Edge e){
+       return v.opposite(e);
+}
+    public void divideByTwo(Vertex u,Vertex v){
+        for (Edge e:u.getOutEdges()){
+            if(e.getEnd().equals(v)){
+                removeEdge(u,v);
+
+
+                Vertex newV=addVertex(u.getData()+v.getData());
+                addEdge(u,newV,e.getWeight()/2);
+                addEdge(v,newV,e.getWeight()/2);
+                System.out.println("After divideByTwo("+u.getData()+","+v.getData()+")");
+                print();
+                printIn();
+                break;
+            }
+        }
+    }
+    private ArrayList<Vertex> element=new ArrayList<>();
+    public void path( Vertex src){
+   
 
     }
-    public void addEdgeIn(Vertex v2){
-        Edge e=new Edge(this,v2);
-        inEdge.add(e);
-    }
-    public void show(){
 
-    }
-    public void removeEdge(Vertex v2){
 
-    }
+
 }
